0e3f580279dfbf79edace992d9759e98d59d00a7
Rafaela de Moraes Papale <rafaela.papale@gec.inatel.br>
2022-04-07 20:08:58 -0300

    :lipstick: Estilização da Dashboard

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,130 @@
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MuiDrawer from '@mui/material/Drawer';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PeopleIcon from '@mui/icons-material/People';
+import List from '@mui/material/List';
+import Avatar from '@mui/material/Avatar';
+
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '../../assets/images/logo.png';
+
+const drawerWidth = 260;
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    '& .MuiDrawer-paper': {
+      position: 'relative',
+      whiteSpace: 'nowrap',
+      width: drawerWidth,
+      overflowX: "hidden",
+      backgroundColor: "#d14600",
+      transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      boxSizing: 'border-box',
+      ...(!open && {
+        overflowX: 'hidden',
+        transition: theme.transitions.create('width', {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.leavingScreen,
+        }),
+        width: theme.spacing(7),
+        [theme.breakpoints.up('sm')]: {
+          width: theme.spacing(9),
+        },
+      }),
+    },
+  }),
+);
+
+export default function Sidebar() {
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
+
+  const styles = {
+    boxPrincipal: {
+      display: 'flex',
+      m: 0,
+      p: 0,
+    },
+
+    boxAvatar: {
+      display: 'flex',
+      justifyContent: 'center',
+    },
+
+    avatarAberto: {
+      width: '150px',
+      height: '150px',
+    },
+
+    avatarFechado: {
+      width: '60px',
+      height: '60px',
+    },
+
+    listItemButton: {
+      backgroundColor: '#b03b00',
+      height: '55px',
+      '&:hover, &:selected, &:focused': {
+        backgroundColor: '#de6021',
+      }
+    },
+
+    color: {
+      color: 'white',
+    }
+  };
+
+  return (
+    <Box sx={styles.boxPrincipal}>
+      <Drawer variant="permanent" open={open} onClick={toggleDrawer}>
+        <Box sx={styles.boxAvatar}>
+          {open === true ?
+            <Avatar
+              alt="Remy Sharp"
+              src={logo}
+              sx={styles.avatarAberto}
+            />
+            :
+            <Avatar
+              alt="Remy Sharp"
+              src={logo}
+              sx={styles.avatarFechado}
+            />
+          }
+        </Box>
+        <List>
+          <Link to="/dashboard">
+            <ListItemButton disableRipple sx={styles.listItemButton}>
+              <ListItemIcon>
+                <DashboardIcon sx={styles.color} />
+              </ListItemIcon>
+              <ListItemText primary="Dashboard" sx={styles.color} />
+            </ListItemButton>
+          </Link>
+        </List>
+        <List>
+          <Link to="/profile">
+            <ListItemButton disableRipple sx={styles.listItemButton}>
+              <ListItemIcon>
+                <PeopleIcon sx={styles.color} />
+              </ListItemIcon>
+              <ListItemText primary="Minha conta" sx={styles.color} />
+            </ListItemButton>
+          </Link>
+        </List>
+      </Drawer>
+    </Box>
+  )
+}
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,5 +1,152 @@
-export default function Dashboard(){
-  return(
-    <h1>OIOIOIOIOIOIOIOIOIO</h1>
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+import Sidebar from '../../components/Sidebar';
+import { Button } from '@mui/material';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: '#ffd000',
+    color: 'black',
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+export default function Dashboard() {
+  const styles = {
+    txtField: {
+      width: '350px',
+      backgroundColor: 'white !important',
+      color: 'white !important',
+      '& label.Mui-focused': {
+        color: '#ba0041',
+        backgroundColor: 'transparent !important',
+      },
+      '& .MuiOutlinedInput-root': {
+        borderRadius: '10px',
+        backgroundColor: 'white !important',
+        '&.Mui-focused fieldset': {
+          backgroundColor: 'transparent !important',
+          borderRadius: '10px',
+          borderColor: '#ba0041',
+        },
+      },
+    }
+  }
+  return (
+    <Box sx={{ width: '100%', height: '100%', display: 'flex', flexDirection: 'row', m: 0, p: 0 }}>
+      <Sidebar />
+      <Box sx={{ width: '100%', height: '100%', p: '24px' }}>
+        <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+          <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+            <TextField id="outlined-basic"
+              label="Meta diária"
+              variant="outlined"
+              disableRipple
+              sx={styles.txtField}
+              InputProps={{
+                endAdornment: <InputAdornment position="end">calorias</InputAdornment>,
+              }}
+            />
+            <Button disableRipple sx={{
+              backgroundColor: '#ba0041',
+              color: 'white',
+              borderRadius: '10px',
+              width: '120px',
+              height: '56px',
+              ml: '24px',
+              '&:hover': {
+                backgroundColor: '#9c0036',
+              }
+            }}>Alterar</Button>
+          </Box>
+          <Box>
+            <TextField id="outlined-basic"
+              label="Total consumido"
+              variant="outlined"
+              disableRipple
+              sx={styles.txtField}
+              InputProps={{
+                endAdornment: <InputAdornment position="end">calorias</InputAdornment>,
+              }}
+            />
+          </Box>
+        </Box>
+        <Box sx={{ display: 'flex', justifyContent: 'end', pt: '24px' }}>
+          <Button disableRipple sx={{
+            backgroundColor: '#ba0041',
+            color: 'white',
+            borderRadius: '10px',
+            width: '350px',
+            height: '56px',
+            ml: '24px',
+            '&:hover': {
+              backgroundColor: '#9c0036',
+            }
+          }}>Novo consumo</Button>
+        </Box>
+        <Box sx={{ pt: '24px' }}>
+          <TableContainer component={Paper} sx={{ borderRadius: '10px' }}>
+            <Table sx={{ minWidth: 700 }} aria-label="customized table">
+              <TableHead>
+                <TableRow>
+                  <StyledTableCell align="center">Dessert (100g serving)</StyledTableCell>
+                  <StyledTableCell align="center">Calories</StyledTableCell>
+                  <StyledTableCell align="center">Fat&nbsp;(g)</StyledTableCell>
+                  <StyledTableCell align="center">Carbs&nbsp;(g)</StyledTableCell>
+                  <StyledTableCell align="center">Protein&nbsp;(g)</StyledTableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows.map((row) => (
+                  <StyledTableRow key={row.name}>
+                    <StyledTableCell component="th" scope="row" align="center">
+                      {row.name}
+                    </StyledTableCell>
+                    <StyledTableCell align="center">{row.calories}</StyledTableCell>
+                    <StyledTableCell align="center">{row.fat}</StyledTableCell>
+                    <StyledTableCell align="center">{row.carbs}</StyledTableCell>
+                    <StyledTableCell align="center">{row.protein}</StyledTableCell>
+                  </StyledTableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Box>
+      </Box>
+    </Box>
   )
 }
